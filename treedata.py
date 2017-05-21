d66c298415c84f6bbdec104c3360a2ea0df132ff
Ron Hsiao <blurryeyes14@gmail.com>
2017-05-21 16:59:25 +0800

    jieba

--- treedata.py
+++ treedata.py
@@ -1,7 +1,15 @@
 import csv
 
+x=1
+f = open('D:\\Shared\\Rawdata\\Split\\Clean\\unix\\numsplittotal.csv', 'a', encoding='UTF-8')
+end=[]
 with open('D:\\Shared\\Rawdata\\Split\\Clean\\unix\\postsplittotal.csv', encoding='UTF-8') as rawcsv:
     readCSV = csv.reader(rawcsv, delimiter=',')
     for row in readCSV:
-        print(len(row))
+        row[0]=x
+        x+=1
+        end.append(row)
+w = csv.writer(f)
+w.writerows(end)
 rawcsv.close()
+f.close()
