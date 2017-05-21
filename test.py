f4265a5f0f74ed8d9b2e633db67fc6ffaa9e2947
Ron Hsiao <blurryeyes14@gmail.com>
2017-05-21 14:43:36 +0800

    jieba

--- test.py
+++ test.py
@@ -6,27 +6,20 @@ with open('D:\\Shared\\224\\AI.csv', encoding='UTF-8') as rawcsv:
     readCSV = csv.reader(rawcsv, delimiter=',')
     for row in readCSV:
         all.append(row)
+rawcsv.close()
 with open('D:\\Shared\\224\\IOT.csv', encoding='UTF-8') as rawcsv2:
     readCSV2 = csv.reader(rawcsv2, delimiter=',')
     for row2 in readCSV2:
         all.append(row2)
-# eee= 0   #看內容
-# for www in all:
-#     print(all[eee])
-#     eee+=1
-
-b = [[0 for m in range(604)] for n in range(51)]
+rawcsv2.close()
+b = [["N" for m in range(371)] for n in range(51)]
 c = ["num", "type"]
-with open('D:\\Shared\\224\\test.csv', encoding='UTF-8') as rawcsv3:
+with open('D:\\Shared\\224\\word.csv', encoding='UTF-8') as rawcsv3:
     readCSV3 = csv.reader(rawcsv3, delimiter=',')
     for row3 in readCSV3:
         c.append(row3[0])
-# oo = 1
-# for tt in range(1, 602):
-#     c.append(oo)
-#     oo += 1
+rawcsv3.close()
 b[0] = c
-#
 i = 0
 for x1 in all:
     b[i + 1][0] = all[i][0]
@@ -35,61 +28,29 @@ for x1 in all:
 j = 0
 hm = {}
 for x2 in b[0]:
-    hm[j] = b[0][j]
+    hm[b[0][j]] = j
     j += 1
+yes = "Y"
 k = 0
-yes = 1
-for x3 in all:
+f = open('D:\\Shared\\224\\tree.csv', 'a', encoding='UTF-8')
+end = []
+end.append(b[0])
+for x3 in range(len(all)):
     h = 2
-    for x31 in range(len(all[0]) - 3):
-        yy = all[k][h]
-        for yy in c :
-            mm=0
-            if yy!=[mm]:
-                mm+=1
-                continue
+    for x31 in range(len(all[k]) - 2):
+        mm = 2
+        for x32 in range(len(c) - 2):
+            yy = all[k][h]
+            if yy != c[mm]:
+                mm += 1
             else:
                 zz = hm[yy]
                 b[k + 1][zz] = yes
-                h += 1
-        print(b[k])
-        k += 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# x = 0
-# y = 0
-# z = 1
-# q = 0
-# for asd in a:
-#     xxx = [9]
-#     for qwe in range(1, 10):
-#         if a[x][q] == z:
-#             xxx[y] = 1
-#             print(a[x][z])
-#             print("T" + str(z))
-#             z += 1
-#             q += 1
-#         else:
-#             xxx[y] = 0
-#             print(a[x][q])
-#             print("F" + str(z))
-#             z += 1
-#             q += 1
-#     x += 1
-#     y += 1
-#     end.append(xxx)
-# print(end)
+                mm += 1
+        h += 1
+    print(b[k + 1])
+    end.append(b[k + 1])
+    k += 1
+w = csv.writer(f)
+w.writerows(end)
+f.close()
